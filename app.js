d4b98971c7d60604e882d37d7c06fd95366bed87
Yash Dixit <yashdixitsq@gmail.com>
2022-08-23 23:38:13 +0530

    [feat] the best code ever written

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+
+app.get('*', (req,res)=>{
+	res.status(200).send({
+		"me": "Meri NFT acchi si",
+		"description": "this shows your position in the AR.",
+		"image": "https://storage.googleapis.com/opensea-prod.appspot.com/creature/50.png"
+	});
+});  
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
